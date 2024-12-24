7b6c2c255615c62893d51cea6a1be58f2249c90b
joel.wright.cs@gmail.com <joel.wright.cs@gmail.com>
2024-12-24 19:21:22 +0000

    Add project files.

---
+++ AdventOfCode2015/Day1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2015
+{
+    internal static class Day1
+    {
+        internal static int CountFloors()
+        {
+            string input = File.ReadAllText("Day1.txt");
+            int floor = 0;
+            foreach (char c in input)
+            {
+                if (c == '(')
+                {
+                    floor++;
+                }
+                else
+                {
+                    floor--;
+                }
+            }
+            return floor;
+        }
+    }
+}
---
+++ AdventOfCode2015/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AdventOfCode2015
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Day1.CountFloors());
+        }
+    }
+}
